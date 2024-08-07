436abd64762cbdaf5ef75af59d8bac9102546bb0
Wayne Yang <g951753321@gmail.com>
2024-08-07 21:41:09 +0800

    test(hardware): Light the LEDs with PWM controller.

--- apps/PID_control/src/main.c
+++ apps/PID_control/src/main.c
@@ -7,6 +7,7 @@
 #include <zephyr/devicetree.h>
 #include <zephyr/sys/printk.h>
 #include <zephyr/device.h>
+#include <zephyr/devicetree.h>
 #include <zephyr/drivers/pwm.h>
 #include <app/drivers/joystick.h>
 
@@ -23,6 +24,13 @@ static const struct pwm_dt_spec pwm_fl = PWM_DT_SPEC_GET(DT_NODELABEL(frontl));
 static const struct pwm_dt_spec pwm_fr = PWM_DT_SPEC_GET(DT_NODELABEL(frontr));
 static const struct pwm_dt_spec pwm_bl = PWM_DT_SPEC_GET(DT_NODELABEL(backl));
 static const struct pwm_dt_spec pwm_br = PWM_DT_SPEC_GET(DT_NODELABEL(backr));
+
+#define DT_SPEC_AND_COMMA(node_id, prop, idx) \
+	PWM_DT_SPEC_GET_BY_IDX(node_id, idx),
+
+static const struct pwm_dt_spec red_leds[] = {
+    DT_FOREACH_PROP_ELEM(DT_NODELABEL(reds), pwms, DT_SPEC_AND_COMMA)
+};
 
 #define MAX_PERIOD PWM_USEC(100U)
 #endif
@@ -54,9 +62,20 @@ int main(void)
                pwm_br.dev->name);
         return 0;
     }
+    for (int i = 0; i < ARRAY_SIZE(red_leds); i++) {
+        if (!pwm_is_ready_dt(&red_leds[i])) {
+            printk("Error: PWM device %s is not ready\n",
+                   red_leds[i].dev->name);
+            return 0;
+        }
+        else
+            printk("PWM device %s is ready\n", red_leds[i].dev->name);
+    }
     struct joystick_data data;
     while(1){
-        joystick_update(joystick_dev, K_MSEC(10));
+        ret = joystick_update(joystick_dev, K_MSEC(10));
+        if (ret != 0)
+            continue;
         data = joystick_get_data(joystick_dev);
         ret = pwm_set_dt(&pwm_fl, MAX_PERIOD, MAX_PERIOD * data.left.x / 1023);
         if (ret) {
@@ -81,6 +100,14 @@ int main(void)
             printk("Error: PWM device %s set failed\n",
                     pwm_br.dev->name);
             return 0;
+        }
+        for (int i = 0; i < ARRAY_SIZE(red_leds); i++) {
+            ret = pwm_set_dt(&red_leds[i], MAX_PERIOD, MAX_PERIOD * ((int16_t *)&data)[i]/ 1023);
+            if (ret) {
+                printk("Error: PWM device %s set failed\n",
+                        red_leds[i].dev->name);
+                return 0;
+            }
         }
     }
 #endif
